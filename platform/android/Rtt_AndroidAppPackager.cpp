kwiksher/Solar2DTux

commit f4a8047da608286b39319d41097dee10d226a5e2

Linux - Sumulator refined use of mktemp and mkdtmp based on OS
	If linux then use the better function mkdtmp else mktmp

platform/android/Rtt_AndroidAppPackager.cpp
@@ -240,8 +240,11 @@ AndroidAppPackager::Build( AppPackagerParams * params, const char * tmpDirBase )
 	char tmpDir[kDefaultNumBytes]; Rtt_ASSERT( kDefaultNumBytes > ( strlen( tmpDirBase ) + strlen( tmpTemplate ) ) );
 	snprintf(tmpDir, kDefaultNumBytes, "%s" LUA_DIRSEP "%s", tmpDirBase, tmpTemplate);
 
-    // This is not as foolproof as mkdtemp() but has the advantage of working on Win32
-    if ( mkdir( mkdtemp(tmpDir) ) )
+#ifdef Rtt_LINUX_ENV
+	if (mkdir(mkdtemp(tmpDir) ) )
+#else
+	if (mkdir(mktemp(tmpDir) ) )
+#endif
 	{
 		char* inputFile = Prepackage( params, tmpDir );
 

platform/shared/Rtt_LinuxAppPackager.cpp
@@ -152,9 +152,11 @@ int LinuxAppPackager::Build(AppPackagerParams* _params, const char* tmpDirBase)
 		debugBuildProcess = (int) strtol(debugBuildProcessPref.GetString(), (char **)NULL, 10);
 	}
 
-
-	// This is not as foolproof as mkdtemp() but has the advantage of working on Win32
+#ifdef Rtt_LINUX_ENV
 	if (mkdir(mkdtemp(tmpDir)) == false)
+#else
+	if (mkdir(mktemp(tmpDir)) == false)
+#endif
 	{
 		// Note that the failing mkdir() that brought us here is a member of the AndroidAppPackager class
 		String tmpString;

platform/shared/Rtt_WebAppPackager.cpp
@@ -260,9 +260,14 @@ int WebAppPackager::Build(AppPackagerParams* params, const char* tmpDirBase)
 		debugBuildProcess = (int) strtol(debugBuildProcessPref.GetString(), (char **)NULL, 10);
 	}
 
+	char* tmp;
 
-	// This is not as foolproof as mkdtemp() but has the advantage of working on Win32
-	char* tmp = mkdtemp(tmpDir);
+#ifdef Rtt_LINUX_ENV
+	tmp = mkdtemp(tmpDir);
+#else
+	tmp = mktemp(tmpDir);
+#endif
+
 	if (Rtt_MakeDirectory(tmp) == false)
 	{
 		// Note that the failing mkdir() that brought us here is a member of the AndroidAppPackager class

commit 82246d93c099842c832f2a343f5de72b1be98233

General/Linux: Fix crash caused by commit f4a8047da608286b39319d41097dee10d226a5e2

platform/android/Rtt_AndroidAppPackager.cpp
@@ -239,12 +239,15 @@ AndroidAppPackager::Build( AppPackagerParams * params, const char * tmpDirBase )
     const char tmpTemplate[] = "CLtmpXXXXXX";
 	char tmpDir[kDefaultNumBytes]; Rtt_ASSERT( kDefaultNumBytes > ( strlen( tmpDirBase ) + strlen( tmpTemplate ) ) );
 	snprintf(tmpDir, kDefaultNumBytes, "%s" LUA_DIRSEP "%s", tmpDirBase, tmpTemplate);
+	bool success = 
 
 #ifdef Rtt_LINUX_ENV
-	if (mkdir(mkdtemp(tmpDir) ) )
+	success = mkdir(mkdtemp(tmpDir));
 #else
-	if (mkdir(mktemp(tmpDir) ) )
+	success = mkdir(mktemp(tmpDir));
 #endif
+
+	if (success)
 	{
 		char* inputFile = Prepackage( params, tmpDir );
 

platform/shared/Rtt_LinuxAppPackager.cpp
@@ -152,11 +152,15 @@ int LinuxAppPackager::Build(AppPackagerParams* _params, const char* tmpDirBase)
 		debugBuildProcess = (int) strtol(debugBuildProcessPref.GetString(), (char **)NULL, 10);
 	}
 
+	bool success = false;
+
 #ifdef Rtt_LINUX_ENV
-	if (mkdir(mkdtemp(tmpDir)) == false)
+	success = mkdir(mkdtemp(tmpDir));
 #else
-	if (mkdir(mktemp(tmpDir)) == false)
+	success = mkdir(mktemp(tmpDir));
 #endif
+	
+	if (!success)
 	{
 		// Note that the failing mkdir() that brought us here is a member of the AndroidAppPackager class
 		String tmpString;