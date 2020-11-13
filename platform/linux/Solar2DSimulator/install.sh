e410e3c86a7f14e2e85cf41abe8c0489020a1b63
Danny Glover <notuxnobux@yahoo.com>
2020-11-13 19:40:10 +0000

    Linux: More android fixes
    - Fix sdk.dir in local.properties (all).
    - Fix permissions on /Android directory.

--- platform/linux/Solar2DSimulator/install.sh
+++ platform/linux/Solar2DSimulator/install.sh
@@ -169,6 +169,7 @@ fi
 # set permissions
 sudo chown $USER:$USER $OPT_LOCATION
 sudo chown $USER:$USER $SAMPLE_CODE_DIR
+sudo chown $USER:$USER $ANDROID_SDK_DIR
 sudo chmod -R a+rwx $OPT_LOCATION/start.sh
 
 echo "In order to build for Android, you need to install Android Studio, install Android Api level 28 via the SDK manager and accept the license agreements."
