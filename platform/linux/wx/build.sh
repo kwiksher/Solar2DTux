3fbbbbd236f8a8e181457a1b1bfaf9932d81e093
Naoya Yamamoto <yamamoto@kwiksher.com>
2020-12-14 15:15:02 +0900

    removed the external folders like CryptoPP, openssl from platform/linux

--- platform/linux/wx/build.sh
+++
@@ -1,5 +0,0 @@
-./configure --with-opengl --with-gtk3 --disable-shared --enable-monolithic --enable-webview --enable-webviewwebkit --enable-mediactrl
-
-sudo apt-get install freeglut3-dev
-sudo apt-get install libglw1-mesa libglw1-mesa-dev
-
