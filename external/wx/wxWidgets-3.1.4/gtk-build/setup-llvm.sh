db9fa96b49f3a23f17b3a6803a4bd1dcca06d862
Naoya Yamamoto <yamamoto@kwiksher.com>
2020-12-14 15:19:52 +0900

    build wx with llvm

---
+++ external/wx/wxWidgets-3.1.4/gtk-build/setup-llvm.sh
@@ -0,0 +1,5 @@
+BASE=$HOME/Documents/work/Solar2DTux/tools/llvm/bin
+SYSROOT="$HOME/Documents/work/sysroot"
+TARGET=arm-linux-gnueabihf
+
+../configure --prefix=$(pwd)/../../../../platform/linux --with-opengl --disable-shared --enable-monolithic --enable-webview --enable-webviewwebkit --enable-mediactrl --enable-graphics_ctx --without-libtiff --host=${TARGET} CXX="${BASE}/${TARGET}-clang++"  CC="${BASE}/${TARGET}-clang" CXXFLAGS="-O2 -g3  -std=c++11"  CFLAGS="" PKG_CONFIG_PATH=${SYSROOT}/usr/lib/arm-linux-gnueabihf/pkgconfig:${SYSROOT}/usr/share/pkgconfig AR="/usr/local/opt/llvm/bin/llvm-ar" AROPTIONS="rcu"
