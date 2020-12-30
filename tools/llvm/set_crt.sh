419e69ee1970a2f1893fcd922fb6193220a8bb47
Naoya Yamamoto <yamamoto@kwiksher.com>
2020-12-30 21:06:19 +0900

    Fixing for aarch64

--- tools/llvm/set_crt.sh
+++ tools/llvm/set_crt.sh
@@ -1,4 +1,8 @@
+SYSROOT="$HOME/Documents/work/sysroot_nano"
+TARGET=aarch64-linux-gnu
+LIBPATH=$HOME/Documents/work/gcc-4.8.5-aarch64/lib/gcc/aarch64-unknown-linux-gnu/4.8.5
+
 ln -s ${LIBPATH}/crtbegin.o ${SYSROOT}/usr/lib/crtbegin.o
 ln -s ${LIBPATH}/crtend.o ${SYSROOT}/usr/lib/crtend.o
-ln -s ${LIBPATH}/usr/lib/gcc/arm-linux-gnueabihf/8/crtbeginS.o
-ln -s ${LIBPATH}/usr/lib/gcc/arm-linux-gnueabihf/8/crtendS.o
+ln -s ${LIBPATH}/usr/lib/gcc/${TARGET}/8/crtbeginS.o
+ln -s ${LIBPATH}/usr/lib/gcc/${TARGET}/8/crtendS.o
